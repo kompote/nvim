2abcb00265109151b6ecbdc4c54876bcfdb9dee6
Thomas FOURCROY <thomas.fourcroy@ciose.fr>
2023-03-14 16:40:25 +0100

    initial

---
+++ after/plugin/colors.lua
@@ -0,0 +1,49 @@
+require("catppuccin").setup({
+    flavour = "mocha", -- latte, frappe, macchiato, mocha
+    background = { -- :h background
+        light = "latte",
+        dark = "mocha",
+    },
+    transparent_background = false,
+    show_end_of_buffer = false, -- show the '~' characters after the end of buffers
+    term_colors = false,
+    dim_inactive = {
+        enabled = false,
+        shade = "dark",
+        percentage = 0.15,
+    },
+    no_italic = false, -- Force no italic
+    no_bold = false, -- Force no bold
+    styles = {
+        comments = { "italic" },
+        conditionals = { "italic" },
+        loops = {},
+        functions = {},
+        keywords = {},
+        strings = {},
+        variables = {},
+        numbers = {},
+        booleans = {},
+        properties = {},
+        types = {},
+        operators = {},
+    },
+    color_overrides = {},
+    custom_highlights = {},
+    integrations = {
+        cmp = true,
+        gitsigns = false,
+        nvimtree = false,
+        telescope = true,
+        notify = false,
+        mini = false,
+        harpoon = true,
+        leap = true,
+        treesitter_context = true,
+        treesitter = true
+        -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
+    },
+})
+
+-- setup must be called before loading
+vim.cmd.colorscheme "catppuccin"
---
+++ lua/tfo/init.lua
@@ -0,0 +1,3 @@
+require("tfo.remap")
+require("tfo.set")
+
