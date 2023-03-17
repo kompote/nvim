ad5ca4f044f46e95bb7b1b275fedc9d64ab8a62b
Thomas Fourcroy <thomas.fourcroy@ciose.fr>
2023-03-17 17:06:58 +0100

    Ajout conf python lsp

--- lua/core/plugin_config/lsp.lua
+++ lua/core/plugin_config/lsp.lua
@@ -1,6 +1,6 @@
 require("mason").setup()
 require("mason-lspconfig").setup({
-    ensure_installed = {"lua_ls"}
+    ensure_installed = {"lua_ls", "pylsp"}
 })
 
 local on_attach = function(_, _)
@@ -15,6 +15,10 @@ end
 
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
+require("lspconfig").pylsp.setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+}
 require("lspconfig").lua_ls.setup {
     on_attach = on_attach,
     capabilities = capabilities,
