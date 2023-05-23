98d7fbc491a2cef69573a2b019d6153905b0cf4a
kompote <tomtomfatcat@gmail.com>
2023-05-23 19:01:23 +0200

    Ajout du script d'auto install dans un container

---
+++ new_install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+./nvim.appimage --appimage-extract
+
+# Optional: exposing nvim globally.
+mv squashfs-root /
+ln -s /squashfs-root/AppRun /usr/bin/nvim
+rm -f nvim.appimage
