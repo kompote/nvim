f206e7dc449dfba7aa876cde1b6e344a4c8537db
Thomas Fourcroy <thomas.fourcroy@ciose.fr>
2023-08-28 12:28:57 +0200

    changement de theme

--- lua/core/plugin_config/lualine.lua
+++ lua/core/plugin_config/lualine.lua
@@ -1,5 +1,15 @@
 require('lualine').setup {
     options = {
         theme = "catppuccin"
-    }
-}
+    },
+            winbar = {
+                lualine_a = {
+                    { "filename", file_status = false, path = 1 },
+                },
+                lualine_b = {},
+                lualine_c = {},
+                lualine_x = {},
+                lualine_y = {},
+                lualine_z = {},
+            },
+        }
